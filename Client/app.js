d7207204349ffd72fcb86347eeb393c3a71ad46c
kylej <krohde13@gmail.com>
2019-09-11 10:41:24 -0500

    adding a table of movies to the index

--- Client/app.js
+++ Client/app.js
@@ -3,10 +3,11 @@
         var dict = {
         	Title : this["title"].value,
         	Director: this["director"].value
+            Genre: this["genre"].value
         };
 
         $.ajax({
-            url: 'https://localhost:44352/api/movie',
+            url: 'https://localhost:44352/api/Movie',
             dataType: 'json',
             type: 'post',
             contentType: 'application/json',
@@ -22,5 +23,42 @@
         e.preventDefault();
     }
 
-    $('#my-form').submit( processForm );
-})(jQuery);
+
+$("tr:has(td)").remove();
+
+$.each(Movie, function (index, Movie) {
+
+    var td_title = $("<td/>");
+
+    // 2.3 get each category of this article
+    $.each(Movie.Title, function (i, Title) {
+        var span = $("<span/>");
+        span.text(title);
+        td_title.append(span);
+    });
+
+    var td_genre = $("<td/>");
+
+    // 2.3 get each category of this article
+    $.each(Movie.Genre, function (i, Genre) {
+        var span = $("<span/>");
+        span.text(Genre);
+        td_genre.append(span);
+    });    var td_genre = $("<td/>");
+
+    var td_director = $("<td/>");
+
+    // 2.3 get each category of this article
+    $.each(Movie.Director, function (i, Director) {
+        var span = $("<span/>");
+        span.text(Director);
+        td_director.append(span);
+    });
+
+    // 2.6 Create a new row and append 3 columns (title+url, categories, tags)
+    $("#movies").append($('<tr/>')
+            .append(td_title)
+            .append(td_director)
+            .append(td_tags)
+    ); 
+}); 
